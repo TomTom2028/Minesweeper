6c811363b9aaefc7fc04761a069097444101f6ef
robbe hovaert <robbe.hovaert@vlot.be>
2020-05-21 23:21:00 +0200

    begon writing cell class

---
+++ GameLogic/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameLogic
+{
+    public class Gameboard
+    {
+        bool isBomb { get; set; }
+
+        int threatNumber { get; set; }
+
+        Panel attachedPanel;
+
+        public Gameboard(Panel attachedPanel)
+        {
+            this.isBomb = false;
+            this.attachedPanel = attachedPanel;
+            threatNumber = 0;
+        }
+
+        public void Show()
+        {
+            if (this.isBomb)
+            {
+
+            }
+        }
+    }
+}
--- Minesweeper/MainForm.cs
+++ Minesweeper/MainForm.cs
@@ -5,16 +5,17 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Minesweeper
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
-            InitializeComponent();
+           InitializeComponent()
         }
     }
 }
